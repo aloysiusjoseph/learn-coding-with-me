c6f425f539671f310fc3f8dbb84432ed1c5447ea
aloysiusjoseph <aloyanimate.info@gmail.com>
2026-09-02 03:37:20 +0100

    delete

---
+++ index/src/index.js
@@ -0,0 +1,7 @@
+favouriteMovieGenre("cowboy")
+favouriteFruit("orange")
+favouritMode("dark")
+favouriteEdgeStyle("sharp")
+function setProp(prop, value) {
+    document.documentElement.style.setProperty(prop.value)
+}
---
+++ python learn/app.py
@@ -0,0 +1,8 @@
+#this is a comment
+print("hello World")
+
+#caculate this
+FirstValue=40
+SecondValue=30
+answer=FirstValue+SecondValue
+print(answer)
